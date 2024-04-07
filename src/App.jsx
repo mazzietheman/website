1d7a563fb12f8d71f031d1de7c2ad894a9766348
cayden nicholas <caydennicholas@gmail.com>
2024-04-07 23:45:37 +0700

    website

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,53 @@
-import * as React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Dashboard from "./Dashboard";
-import Page1 from "./components/page1";
-import Page2 from "./components/page2";
-import ProductList from "./components/productList";
-import ProductForm from "./components/productForm";
-import Login from "./Login";
-import { AuthProvider } from "./context/AuthContext";
-import { ProtectedRoute } from "./ProtectedRoute";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <ProtectedRoute>
-        <Dashboard />
-      </ProtectedRoute>
-    ),
-    children: [
-      {
-        path: "page-one",
-        element: <Page1 />,
-      },
-      {
-        path: "page-two",
-        element: <Page2 />,
-      },
-      {
-        path: "product-list",
-        element: <ProductList />,
-      },
-      {
-        path: "editproducts/:id",
-        element: <ProductForm />,
-      },
-      ,
-      {
-        path: "add-product",
-        element: <ProductForm />,
-      },
-    ],
-  },
-  {
-    path: "login",
-    element: <Login />,
-  },
-]);
+import * as React from "react"; 
+import { createBrowserRouter, RouterProvider } from "react-router-dom"; 
+import Dashboard from "./Dashboard"; 
+import Page1 from "./components/page1"; 
+import Page2 from "./components/page2"; 
+import ProductList from "./components/productList"; 
+import ProductForm from "./components/productForm"; 
+import Login from "./Login"; 
+import { AuthProvider } from "./context/AuthContext"; 
+import { ProtectedRoute } from "./ProtectedRoute"; 
+ 
+const router = createBrowserRouter([ 
+    { 
+        path: "/", 
+        element: ( 
+            <ProtectedRoute> 
+                <Dashboard /> 
+            </ProtectedRoute> 
+        ), 
+        children: [ 
+            { 
+                path: "page-one", 
+                element: <Page1 />, 
+            }, 
+            { 
+                path: "page-two", 
+                element: <Page2 />, 
+            }, 
+            { 
+                path: "product-list", 
+                element: <ProductList />, 
+            }, 
+            { 
+                path: "editproducts/:id", 
+                element: <ProductForm />, 
+            }, 
+            , 
+            { 
+                path: "add-product", 
+                element: <ProductForm />, 
+            }, 
+        ], 
+    }, 
+    { 
+        path: "login", 
+        element: <Login />, 
+    }, 
+]); 
+ 
+export default function App() { 
+    return ( 
+        <AuthProvider> 
+            <RouterProvider router={router} />
